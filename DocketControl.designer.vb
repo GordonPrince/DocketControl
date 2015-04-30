2c084992f04ad46db40840b3f39113e3bcb8efe7
Gordon Prince <gordon.prince@tekhelps.onmicrosoft.com>
2015-04-30 08:22:55 -0500

    published to run 4/30

--- DocketControl.designer.vb
+++ DocketControl.designer.vb
@@ -201,7 +201,7 @@
         Me.Label1.RightToLeft = System.Windows.Forms.RightToLeft.No
         Me.Label1.Size = New System.Drawing.Size(181, 21)
         Me.Label1.TabIndex = 11
-        Me.Label1.Text = "2015-Apr-29  10:20"
+        Me.Label1.Text = "2015-Apr-29  10:40"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.BottomRight
         '
         'chkShowMessages
